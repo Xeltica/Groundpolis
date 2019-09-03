7c4da21ec94af7d76a5fe0ef3e652e424de971e8
Xeltica <xeltica@gmail.com>
2019-09-03 12:03:42 +0900

    Rewrite app.vue

--- src/client/app/app.vue
+++ src/client/app/app.vue
@@ -1,32 +1,30 @@
 <template>
-<router-view id="app" v-hotkey.global="keymap"></router-view>
+	<router-view id="app" v-hotkey.global="keymap"></router-view>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import { Vue, Component } from 'vue-property-decorator';
 import { url, lang } from './config';
 
-export default Vue.extend({
-	computed: {
-		keymap(): any {
-			return {
-				'h|slash': this.help,
-				'd': this.dark
-			};
-		}
-	},
-
-	methods: {
-		help() {
-			window.open(`${url}/docs/${lang}/keyboard-shortcut`, '_blank');
-		},
-
-		dark() {
-			this.$store.commit('device/set', {
-				key: 'darkmode',
-				value: !this.$store.state.device.darkmode
-			});
-		}
+@Component
+export default class App extends Vue {
+	public get keymap() {
+		return {
+			'h|slash': this.help,
+			d: this.dark
+		};
 	}
-});
+
+	public help() {
+		window.open(`${url}/docs/${lang}/keyboard-shortcut`, '_blank');
+	}
+
+	public dark() {
+		this.$store.commit('device/set', {
+			key: 'darkmode',
+			value: !this.$store.state.device.darkmode
+		});
+
+	}
+}
 </script>
