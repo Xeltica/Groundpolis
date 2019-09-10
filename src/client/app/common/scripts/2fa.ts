6d9a1e873a20f87bacf513d9d1bbcf6eb7699ced
Xeltica <xeltica@gmail.com>
2019-09-10 15:53:18 +0900

    fix type

--- src/client/app/common/scripts/2fa.ts
+++ src/client/app/common/scripts/2fa.ts
@@ -1,5 +1,5 @@
 export function hexifyAB(buffer) {
 	return Array.from(new Uint8Array(buffer))
-		.map(item => item.toString(16).padStart(2, 0))
+		.map(item => item.toString(16).padStart(2, '0'))
 		.join('');
 }
